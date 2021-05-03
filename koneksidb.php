99e3c1804531315cbbe97f576678812f96462a6d
Ullylaili2710 <79493899+Ullylaili2710@users.noreply.github.com>
2021-05-03 19:59:30 +0700

    Add files via upload

---
+++ koneksidb.php
@@ -0,0 +1,9 @@
+<?php
+// konfigurasi database
+$host = "localhost"; // Nama host
+$user = "root"; // Nama user
+$password =""; // Password
+$database = "formulirdata" // Nama database
+// perintah php untuk akses ke database
+$koneksi = mysqli_connect($host, $user, $password, $database); // Variabel koneksi untuk menyambungkan ke database
+?>
